f7769387951f124fba45816e202a1d493ee142bb
rkozyak <61517945+rkozyak@users.noreply.github.com>
2021-04-08 21:39:26 -0400

    Fixed daily quote :)

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //Imports configs
 require('dotenv').config();
 const config = require('./config.json')
+const quotes = require('./resources/quote.json')
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -23,7 +24,7 @@ for (const file of commandFiles) {
 client.once('ready', () => {
     console.log('Aiko is online!');
     memberCounter(client);
-    sendDaily();
+    //sendDaily();
 });
 
 //Gives new members the pending role and sends a message welcoming them
@@ -74,54 +75,34 @@ client.login(process.env.DISCORD_TOKEN);
 
 
 //Daily Message (WIP)
-async function sendDaily() {
-    const sendDailyMessage = async () => {
-        console.log('sending daily quote...')
-        for (const entry in config.dailyMessage) {
-            const server = config.dailyMessage[entry]
-            const guild = client.guilds.cache.get(server.guildId)
-            const channel = client.channels.cache.get(server.channelId);
-            if (guild && channel) {
-                console.log(`sending daily message to guild ${server.guildId}`)
-                await new Promise((resolve) => {
-                    const selectedQuote = quotes[Math.floor(Math.random() * quotes.length)]
+client.on('ready', () => {
+    setTimeout(function(){ // in leftToEight() milliseconds run this:
+        sendMessage(); // send the message once
+        var dayMillseconds = 1000 * 60 * 60 * 24;
+        setInterval(function(){ // repeat this every 24 hours
+            sendMessage();
+        }, dayMillseconds)
+    }, leftToEight())
+})
 
-                    const newEmbed = new Discord.MessageEmbed({
-                        color: '#0f4e5f',
-                        title: 'Daily Quote :))',
-                        fields: [
-                            { name: `"${selectedQuote.quote}"`, value: `- ${selectedQuote.author}` }
-                        ]
-                    }).setFooter('Made with ❤ by richie#0437').setThumbnail(selectedQuote.thumbnail)
+function leftToEight(){
+    var d = new Date();
+    return (-d + d.setHours(12,0,0,0));
+}
 
+const selectedQuote = quotes[Math.floor(Math.random() * quotes.length)]
+const newEmbed = new Discord.MessageEmbed({
+    color: '#0f4e5f',
+    title: 'Daily Motivational Quote :))',
+    fields: [
+        { name: `"${selectedQuote.quote}"`, value: `- ${selectedQuote.author}` }
+    ]
+}).setFooter('Made with ❤ by richie#0437').setThumbnail(selectedQuote.thumbnail)
 
-                    channel.send(newEmbed).then(() => {
-                        resolve();
-                    })
-                })
-            } else {
-                console.log(`${server.guildId} failed check`);
-            }
-        }
-
-        client.destroy();
+function sendMessage(){
+    var guild = client.guilds.cache.get('824491352153653258');
+    if(guild && guild.channels.cache.get('827930016083738664')){
+        guild.channels.cache.get('827930016083738664').send(newEmbed);
     }
-    //Waits until next midnight
-    await new Promise((resolve) => {
-        const currentTime = Date.now();
-        const nextMidnight = ((Math.ceil(currentTime / 86400000) + 1) * 86400000)
 
-        setTimeout(() => {
-            resolve();
-        }, nextMidnight - currentTime)
-    })
-    //Start cycle
-    while (true) {
-        sendDailyMessage();
-        await new Promise((resolve) => {
-            setTimeout(() => {
-                resolve();
-            }, 86400000)
-        })
-    }
 }
