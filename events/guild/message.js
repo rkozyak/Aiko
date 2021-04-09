9787d4282ad45eee21a11250e358874ced4ca304
rkozyak <61517945+rkozyak@users.noreply.github.com>
2021-04-09 16:31:20 -0400

    Improved music player

--- events/guild/message.js
+++ events/guild/message.js
@@ -6,6 +6,9 @@ module.exports = (Discord, client, message) =>{
     const cmd = args.shift().toLowerCase();
 
     const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd));
-
-    if(command) command.execute(client, message, args, Discord);
+    
+    try {command.execute(client, message, args, Discord, cmd);
+    } catch(err) {
+        message.reply("There was an error trying to execute that command");
+    }
 }
